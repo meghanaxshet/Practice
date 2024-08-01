bd4eaf72105566db5fb58d465801f64deb9de893
Meghana Shet <megharaikar6@gmail.com>
2024-08-01 19:58:59 +0530

    Update PracticeApplication.java

--- src/main/java/com/raikar/PracticeApplication.java
+++ src/main/java/com/raikar/PracticeApplication.java
@@ -8,7 +8,7 @@ public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
-		System.out.println("Started");
+		System.out.println("Started successfully");
 	}
 
 }
